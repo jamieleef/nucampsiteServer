e75a7b67001f365a08371639daf6667cc1fd69f7
Jamie Feliu <jamieleefeliu@gmail.com>
2023-06-28 09:58:54 -0400

    Week 4 Task 2 Update

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -5,8 +5,20 @@ const Favorite = require("../models/favorite");
 
 const favoriteRouter = express.Router();
 
-favoriteRouter.route("/");
+favoriteRouter
+    .route("/")
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+    .get(cors.cors, authenticate.verifyUser, (req, res) => {})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {});
 
-favoriteRouter.route("/:campsiteId");
+favoriteRouter
+    .route("/:campsiteId")
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+    .get(cors.cors, authenticate.verifyUser, (req, res) => {})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {});
 
 module.exports = favoriteRouter;
